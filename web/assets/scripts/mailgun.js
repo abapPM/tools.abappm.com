c3dd32a3f255206bf179e795cf32bd4e95ae16a7
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2025-11-13 10:24:23 -0500

    Update mailgun script

--- web/assets/scripts/mailgun.js
+++ web/assets/scripts/mailgun.js
@@ -22,6 +22,8 @@ document.getElementById('mailgun-form').addEventListener('submit', async functio
     resultDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
     updateCopyButtonVisibility('mailgun-result', 'copy-mailgun-btn');
   } catch (error) {
+    console.log('response', response);
+    console.log('error', error);
     resultDiv.innerHTML = `<span class="error">${error.message}</span>`;
   }
 });
